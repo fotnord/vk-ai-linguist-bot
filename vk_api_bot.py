64993c666c61ef628e3064d6c17dc4e316a4aaaf
fotnord <fotnord@gmail.com>
2026-07-07 20:52:56 +0300

    minor improvements and dialogue history management

--- vk_api_bot.py
+++ vk_api_bot.py
@@ -28,14 +28,17 @@ def ask_ai(user_text: str) -> str:
                         "content": user_text
                     }
                 ],
-                model="llama-3.1-8b-instant"
+                model="llama-3.1-8b-instant",
+                temperature=0.3,
+                max_completion_tokens=1024,
+                top_p=0.3,
             )
             return completion.choices[0].message.content
     except Exception as groq_error:
         print(f"Что-то пошло не так: {groq_error}")
         return "Возникли временные трудности с доступом к ИИ"
 
-# vk part
+# vk part 1
 def start_vk_bot():
     vk_session = vk_api.VkApi(token=VK_TOKEN)
     vk = vk_session.get_api()
@@ -43,26 +46,37 @@ def start_vk_bot():
     
     print("Бот запущен, ожидаю Ваш запрос")
     
+    dict_context = {}
+    max_messages = 5
+    
     try:
         for event in longpoll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW and event.from_user:
                 user_id = event.obj.message['from_id']
                 user_text = event.obj.message['text']
-                print(f"Пользователь {user_id} написал {user_text}")
 
-# response
-                ai_response = ask_ai(user_text)
-               
-                try:
-                    vk.messages.send(
-                        user_id=user_id,
-                        message=ai_response,
-                        random_id=random.randint(1, 100000000))
-                except Exception as vk_error:
-                    print(f"Ошибка отправки сообщения в Вк: {vk_error}")
-    
-    except Exception as general_error:
-        print(f"Критическая ошибка бота: {general_error}")
+                if user_id in dict_context:
+                    history = dict_context.get(user_id)
+                    history.append(user_text)
+                    if len(history) > max_messages:
+                        history.pop(0)
+                    dict_context[user_id] = history
+                else:
+                    dict_context[user_id] = [user_text]
+
+                history_for_llm = " ".join(dict_context[user_id])
+                ai_response = ask_ai(history_for_llm)
+
+# vk part 2
+                vk.messages.send(
+                    user_id=user_id,
+                    message=ai_response,
+                    random_id=random.randint(1, 100000000))
         
+                print(f"Пользователь {user_id} написал {user_text}")
+        
+    except Exception as general_error:
+                print(f"Критическая ошибка бота: {general_error}")
+
 if __name__ == "__main__":
     start_vk_bot()
